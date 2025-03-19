1afb5c0f16ecfbaad1e2267aae4ba0c0840cc425
David Wales <david.wales95@gmail.com>
2025-03-19 15:52:37 -0500

    removed unused color prop

--- src/interface/src/app/components/core/surfaces/app_bar.tsx
+++ src/interface/src/app/components/core/surfaces/app_bar.tsx
@@ -1,21 +1,16 @@
 import { convertToSx } from "@/app/components/core/mui_style_resolvers";
-import { ColorToken, StyleProps } from "@/app/components/core/style_props";
+import { StyleProps } from "@/app/components/core/style_props";
 import { AppBar as MuiAppBar, Toolbar } from "@mui/material";
 
 interface AppBarProps {
     children: React.ReactNode;
     position?: 'fixed' | 'sticky' | 'absolute' | 'static' | 'relative';
-    color?: ColorToken;
     styling?: StyleProps;
 }
 
 export default function AppBar(props: AppBarProps) {
     return (
-        <MuiAppBar
-            position={props.position ?? 'sticky'}
-            color={props.color || 'primary'}
-            sx={props.styling && convertToSx(props.styling)}
-        >
+        <MuiAppBar position={props.position ?? 'sticky'} sx={props.styling && convertToSx(props.styling)}>
             <Toolbar>
                 {props.children}
             </Toolbar>
