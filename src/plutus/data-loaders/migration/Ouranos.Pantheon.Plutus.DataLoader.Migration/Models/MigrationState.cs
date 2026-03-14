468c00ca4b8ec058309a762ce95e2413c42bf2ce
David Wales <david.wales95@gmail.com>
2026-03-14 13:32:27 -0500

    refactor: remove all unrequired references to the legacy mongodb storage solution

--- src/plutus/data-loaders/migration/Ouranos.Pantheon.Plutus.DataLoader.Migration/Models/MigrationState.cs
+++
@@ -1,8 +0,0 @@
-namespace Ouranos.Pantheon.Plutus.DataLoader.Migration.Models;
-
-public sealed record MigrationState(
-    string Id
-)
-{
-    public DateTimeOffset? LastMigratedTradeCreatedAt { get; set; }
-}
